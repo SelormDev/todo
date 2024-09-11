101e74829c23b2f7205b6355cfec2e58517260e8
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-09-11 10:03:22 +0000

    Modified file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-import todo from './modules/todo';
-import app from './modules/ui'
+import ui from './modules/ui'
 import './app.css'
 
-const newTodo = todo('Run 2 miles', 'Run 2 miles around the park', '2021-09-30', 'High', 'Bring water');
-console.log(newTodo.dueDate);
-console.log(app)
+const app = document.getElementsByTagName('body');
+app.appendChild(ui);
--- src/modules/ui/index.js
+++ src/modules/ui/index.js
@@ -1,28 +1,17 @@
 import "./index.css"
-import logo from "./../../images/logo.svg"
+import aside from "./aside"
 
 const body = document.getElementById("app")
 body.classList.add("body")
 
-const aside = document.createElement("div")
-aside.classList.add("aside")
+
 const main = document.createElement("div")
 main.classList.add("main")
 
 body.appendChild(aside)
 body.appendChild(main)
 
-const logoSection = document.createElement("div")
-logoSection.classList.add("logoSection")
-const logoImg = document.createElement("img")
-logoImg.src = logo
-const logoText = document.createElement("h1")
-logoText.innerHTML = "Tasks <br /> Book"
 
-logoSection.appendChild(logoImg)
-logoSection.appendChild(logoText)
-
-aside.appendChild(logoSection)
 
 
 export default body;
