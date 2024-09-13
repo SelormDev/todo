8ed3e7d640942316751da88ea47d2eea67165e12
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-09-13 07:25:20 +0000

    fix: broken link

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import './app.css'
-import nav from './modules/ui/nav'
-import main from './modules/ui/main'
-import aside from './modules/ui/aside'
+import nav from './ui/nav'
+import main from './ui/main'
+import aside from './ui/aside'
 
 const app = document.getElementById('app');
 app.appendChild(nav);
