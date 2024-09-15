f128e19ad07e5b01b9e3f7f74c5478c6dbee65af
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-09-15 19:42:48 +0000

    Create observation card

--- src/ui/main/sections/informationSection/observation/index.js
+++ src/ui/main/sections/informationSection/observation/index.js
@@ -1,16 +1,22 @@
-const observationsObj = {
-    title: 'Observation',
-    content: 'This is the content of the observation section'
-}
+const observationsArr = [{ content: "You created the most tasks on " }, { content: "You completed the most tasks on " }, { title: "Most active day", content: "You were most active on " }, { content: "You were most inactive on " }]
 
 const observation = document.createElement("section")
-observation.classList.add("observation")
+observation.classList.add("observation", "card")
 
-const heading = document.createElement("h2")
-heading.textContent = observationsObj.title
+const heading = document.createElement("h3")
+heading.textContent = "Observations"
 
-const content = document.createElement("p")
-content.textContent = observationsObj.content
+const content = document.createElement("div")
+content.classList.add("observation-content")
+
+observationsArr.forEach(observation => {
+    const observationElement = document.createElement("p")
+    observationElement.textContent = observation.content
+    content.appendChild(observationElement)
+}
+)
+
+
 
 observation.appendChild(heading)
 observation.appendChild(content)
