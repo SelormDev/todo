15d20f76bf14b1d18ef305a578bbd5f4b6fa0777
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-09-15 19:41:06 +0000

    Modify file

--- src/ui/main/index.js
+++ src/ui/main/index.js
@@ -1,8 +1,12 @@
 import "./index.css"
-
+import informationSection from "./sections/informationSection"
+import tasksSection from "./sections/tasksSection"
 
 const main = document.createElement("div")
 main.classList.add("main")
+
+main.appendChild(tasksSection)
+main.appendChild(informationSection)
 
 
 
