15ef57d849f4ed1215fb23ff7725acd96899c3e3
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-09-19 22:51:30 +0000

    Create progress ui

--- src/modules/todo/index.js
+++
@@ -1,6 +0,0 @@
-
-function todo(title, description, dueDate, priority, notes) {
-    return { title, description, dueDate, priority, notes }
-}
-
-export default todo;
--- src/ui/main/sections/tasksSection/index.js
+++ src/ui/main/sections/tasksSection/index.js
@@ -1,11 +1,13 @@
 import "./index.css"
 import { currentTasks, completedTasks } from "./tasks"
+import projects from "./projects"
+import { createProject } from "../../../../modules/createProject"
 
 const tasksSection = document.createElement("section")
 tasksSection.classList.add("tasksSection")
 
 const progress = document.createElement("div")
-progress.classList.add("progress")
+progress.classList.add("progress", "card")
 
 const heading = document.createElement("h3")
 heading.innerHTML = "Progress of the week"
@@ -16,21 +18,35 @@ container.classList.add("progressContainer")
 const tasks = document.createElement("div")
 tasks.classList.add("tasks")
 const tasksHeading = document.createElement("h4")
-tasksHeading.innerHTML = currentTasks.length
+tasksHeading.textContent = "Current Tasks"
+const tasksValue = document.createElement("p")
+tasksValue.textContent = currentTasks.length
 
 const completed = document.createElement("div")
 completed.classList.add("completed")
 const completedHeading = document.createElement("h4")
-completedHeading.innerHTML = completedTasks.length
+completedHeading.textContent = "Completed Tasks"
+const completedValue = document.createElement("p")
+completedValue.textContent = completedTasks.length
+
+const newProject = createProject("New Project")
+projects.push(newProject)
+// console.log(createProject)
 
 const projectsCount = document.createElement("div")
-projectsCount.classList.add("projectsCount")
+projectsCount.classList.add("projects-count")
 const projectsHeading = document.createElement("h4")
-projectsHeading.innerHTML = "0"
+projectsHeading.textContent = "Projects"
+const projectsValue = document.createElement("p")
+projectsValue.textContent = projects.length
+
 
 tasks.appendChild(tasksHeading)
+tasks.appendChild(tasksValue)
 completed.appendChild(completedHeading)
+completed.appendChild(completedValue)
 projectsCount.appendChild(projectsHeading)
+projectsCount.appendChild(projectsValue)
 
 container.appendChild(tasks)
 container.appendChild(completed)
