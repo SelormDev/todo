a489bf7867acd121af04575b98b3cece615f7813
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-09-19 22:50:51 +0000

    Create projects bank

---
+++ src/ui/main/sections/tasksSection/projects.js
@@ -0,0 +1,3 @@
+const projects = [{ name: "Project 1", tasks: [{ name: "Task 1", completed: false }, { name: "Task 2", completed: true }] }, { name: "Project 2", tasks: [{ name: "Task 3", completed: false }, { name: "Task 4", completed: true }] }]
+
+export default projects;
